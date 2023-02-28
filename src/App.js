a92f2ab2f95bf216b70c0d6d13c21ab3065f4a3b
Fakhrul Islam <fakhrulif@gmail.com>
2023-02-28 16:07:29 +0600

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/Home/Framer.js 	new file:   src/Home/FramerTest.js 	new file:   src/Home/MiddleNavbar.js 	new file:   src/Home/ParallaxText.js 	modified:   src/Shared/Navbar.js 	modified:   tailwind.config.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Shared/Navbar';
-import { Route, Routes } from 'react-router-dom';
-import Home from './Home/Home';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Shared/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home/Home";
+import FramerTest from "./Home/FramerTest";
+import MiddleNavbar from "./Home/MiddleNavbar";
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-   <Routes>
-   
-   </Routes>
-   <Routes>
-    <Route path='/' element={<Home/>}/>
-   </Routes>
- 
+    <div className="bg-white">
+      <FramerTest />
+      <MiddleNavbar />
+      <Navbar />
+      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
